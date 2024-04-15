4b37fe55df3fa935de48d2df053737eb7794eaea
Ahmed Ashraf <ahmed_ashraf15@icloud.com>
2024-04-15 16:06:17 +0200

    adding requirements file and exporting model
    adding requirements file , exporting model as pkl file for deployment , adding streamlit.py for deployment in streamlit app

---
+++ streamlit_app/streamlit.py
@@ -0,0 +1,87 @@
+import pickle
+import streamlit as st
+import pandas as pd
+
+
+# Page config
+st.set_page_config(
+    page_title="Bank Loan Classification",
+    page_icon="/Users/ahmedashraf/Downloads/image.jpg",
+)
+
+# Page title
+st.title('Bank Loan - Loan Prediction')
+st.image('/Users/ahmedashraf/Downloads/image.jpg')
+st.write("\n\n")
+
+st.markdown(
+    """
+    This app aims to assist in classifying bank loan, thereby reducing the time required to analyze machine problems. It enables the analysis of sensor data to classify failures swiftly and expedite the troubleshooting process.    
+    """
+)
+
+# Load the model
+with open('/Users/ahmedashraf/Documents/AI roadmaps & Training/IEEE ManCSC/Bank Loan Status Dataset/model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Streamlit interface to input data
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    current_loan_amount = st.number_input(label='Current Loan Amount')
+    term = st.selectbox(label='Term Type', options=["Short Term", "Long Term"])
+    credit_score = st.number_input(label='Credit Score')
+    annual_income = st.number_input(label='Annual Income')
+    years_in_current_job = st.selectbox(label='Years in current job', options=["0-1 year", "2-3 years", "4-6 years",
+                                                                               "7-9 years", "+10 years"])
+
+with col2:
+    home_ownership = st.selectbox(label='Home Ownership', options=["Rent", "Home Mortgage", "Own Home"])
+    purpose = st.selectbox(label='Loan Purpose', options=["Personal", "Debt Consolidation", "Business Loan", "Other"])
+    monthly_debt = st.number_input(label='Monthly Debt')
+    years_of_credit_history = st.number_input(label='Years of Credit History')
+    no_of_open_accounts = st.selectbox(label='Number of open accounts')
+
+with col3:
+    no_of_credit_problems = st.number_input(label='Number of credit problems', options=["0", "1", "2", "3", "4", "5",
+                                                                                        "6", "7"])
+    current_credit_balance = st.number_input(label='Current Credit Balance')
+    max_open_credit = st.number_input(label='Maximum open credit')
+    bankruptcies = st.selectbox(label='Bankruptcies', options=["0", "1", "2", "3", "4", "5"])
+    tax_liens = st.selectbox(label='Tax Liens', options=["0", "1", "2", "3", "4", "5", "6"])
+
+# Function to predict the input
+def prediction(current_loan_amount, term, credit_score, annual_income, years_in_current_job,
+               home_ownership, purpose, monthly_debt, years_of_credit_history,
+               no_of_open_accounts,
+               no_of_credit_problems, current_credit_balance, max_open_credit, bankruptcies, tax_liens):
+    # Create a df with input data
+    df_input = pd.DataFrame({
+        'Current Loan Amount': [current_loan_amount],
+        'Term': [term],
+        'Credit Score': [credit_score],
+        'Annual Income': [annual_income],
+        'Years in current job': [years_in_current_job],
+        'Home Ownership': [home_ownership],
+        'Purpose': [purpose],
+        "Monthly Debt": [monthly_debt],
+        'Years of Credit History': [years_of_credit_history],
+        'Number of Credit Problems': [no_of_credit_problems],
+        'Number of Open Accounts': [no_of_open_accounts],
+        'Current Credit Balance': [current_credit_balance],
+        'Maximum Open Credit': [max_open_credit],
+        'Bankruptcies': [bankruptcies],
+        'Tax Liens': [tax_liens],
+    })
+
+    prediction = model.predict(df_input)
+    return prediction
+
+
+# Botton to predict
+if st.button('Predict'):
+    predict = prediction(current_loan_amount, term, credit_score, annual_income, years_in_current_job,
+               home_ownership, purpose, monthly_debt, years_of_credit_history, no_of_open_accounts,
+               no_of_credit_problems, current_credit_balance, max_open_credit, bankruptcies, tax_liens)
+    st.success(predict)
+    
